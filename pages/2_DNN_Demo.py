8bf3423853ec5b1e57ecfb17f281cc7276650110
reyhanwiira <reyhanwiira@gmail.com>
2024-03-20 23:25:10 +0700

    Update 2_DNN_Demo.py

--- pages/2_DNN_Demo.py
+++ pages/2_DNN_Demo.py
@@ -7,20 +7,20 @@ import torch
 import torch.nn as nn
 import time
 
-# class DNN(nn.Module):
-#     def __init__(self, input_size, hidden_size1, hidden_size2):
-#         super(DNN, self).__init__()
-#         self.fc1 = nn.Linear(input_size, hidden_size1)
-#         self.fc2 = nn.Linear(hidden_size1, hidden_size2)
-#         self.fc3 = nn.Linear(hidden_size2, 1)
-#         self.activation = nn.ReLU()  # Changed activation to ReLU for hidden layers
-#         self.sigmoid = nn.Sigmoid()
+class DNN(nn.Module):
+    def __init__(self, input_size, hidden_size1, hidden_size2):
+        super(DNN, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size1)
+        self.fc2 = nn.Linear(hidden_size1, hidden_size2)
+        self.fc3 = nn.Linear(hidden_size2, 1)
+        self.activation = nn.ReLU()  # Changed activation to ReLU for hidden layers
+        self.sigmoid = nn.Sigmoid()
 
-#     def forward(self, x):
-#         out = self.activation(self.fc1(x))
-#         out = self.activation(self.fc2(out))
-#         out = self.sigmoid(self.fc3(out))
-#         return out
+    def forward(self, x):
+        out = self.activation(self.fc1(x))
+        out = self.activation(self.fc2(out))
+        out = self.sigmoid(self.fc3(out))
+        return out
 
 # Load Our pre-trained model
 PATH = "./pages/churn-model-full.pt"
