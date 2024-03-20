19aa229fe775567b78e0c61dfc46df4cac3e4619
reyhanwiira <31840645+reyhanwiira@users.noreply.github.com>
2024-03-20 11:10:47 +0700

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.write("# Welcome to My Demo App! 👋")
+
+st.sidebar.success("Select an App you want to try.")
+
+st.markdown(
+    """
+    This App was made for fullfilling the requirement to complete the course!
+
+    **👈 Select an App on the left to get started.**
+    
+"""
+)
